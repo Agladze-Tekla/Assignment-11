3268585d7ce06d86618e0595b8beda99afc2bda5
Tekla <tekla@Teklas-Mac.local>
2023-10-29 03:20:15 -0700

    Assignment11

--- Assignment11/ViewController.swift
+++ Assignment11/ViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let signInButton: UIButton = {
+    private let signInButton: UIButton = {
        
-        let button = UIButton()
+        let button = UIButton(type: .system)
+        button.backgroundColor = .purple
         button.setTitle("Sign In", for: .normal)
-        button.backgroundColor = .systemPink
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
+    }()
+    
+    private let loginContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .gray
+        textField.placeholder = "Username"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .gray
+        textField.placeholder = "Password"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .purple
-        signInButton.frame = CGRect(x: 150, y: 100, width: 140, height: 45)
-        view.addSubview(signInButton)
+        view.backgroundColor = .systemPink
+        loginContentView.addSubview(usernameTextField)
+        loginContentView.addSubview(passwordTextField)
+        loginContentView.addSubview(signInButton)
+        view.addSubview(loginContentView)
+        setUpAutoLayout()
         
         signInButton.addAction(UIAction(handler: { [weak self] action in
             self?.navigateToHomePage()
@@ -31,8 +66,35 @@ class ViewController: UIViewController {
         
     }
     
-    func navigateToHomePage() {
+    //MARK: - SetUp Layout
+    private func setUpAutoLayout() {
+        //logInContentView Constraints
+        loginContentView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        loginContentView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        loginContentView.heightAnchor.constraint(equalToConstant: view.frame.height/3).isActive = true
+        loginContentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
+        //UsernameTextField Constraints
+        usernameTextField.topAnchor.constraint(equalTo: loginContentView.topAnchor, constant: 40).isActive = true
+        usernameTextField.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 20).isActive = true
+        usernameTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        usernameTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        //PasswordTextField Constraints
+        passwordTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20).isActive = true
+        passwordTextField.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 20).isActive = true
+        passwordTextField.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -20).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        //SignInButton Constarints
+        signInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 40).isActive = true
+        signInButton.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 20).isActive = true
+        signInButton.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -20).isActive = true
+        signInButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    
+    func navigateToHomePage() {
         let homePage = HomePageVC()
         self.present(homePage, animated: true)
     }
